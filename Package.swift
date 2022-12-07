987d2df245685c73a91a1753659d72a9fb27e547
Water Lou <water@waterworld.com.hk>
2022-12-07 17:29:07 +0800

    update package.swift

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
 
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", upToNextMajor: "4.0.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor("5.2.0"))
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", upToNextMajor(from: "4.0.0")),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
